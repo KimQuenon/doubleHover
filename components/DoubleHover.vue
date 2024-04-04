7d355412a1da370acd84b926acd05ee8d96baa9d
Kim <kimberley.quenon.@gmail.com>
2024-04-04 11:42:10 +0200

    hidden text

--- components/DoubleHover.vue
+++ components/DoubleHover.vue
@@ -3,11 +3,13 @@
         <div class="w-full h-full rounded-lg overflow-hidden relative p-2">
             <div class="bg-white rounded shadow backgroundScale h-full relative">
                 <div class="rounded w-full h-full shadow-lg p-10 z-50 relative">
-                    <div class="w-full h-full">
-                        <h2 class="text-xl font-semibold mb-2">Card 1</h2>
+                    <div class="w-full h-full flex flex-col justify-end">
+                        <h2 class="text-xl font-semibold">Card 1</h2>
                         <p class="text-gray-600">Tag 1</p>
-                        <!-- <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p> -->
-                        <button class="bg-red-400 p-2 px-8 rounded-full text-white mt-4" type="button">See all</button>
+                        <p class="hiddenText text-gray-600 opacity-0 transition-opacity duration-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                        <div>
+                            <button class="bg-red-400 p-2 px-8 rounded-full text-white mt-4" type="button">See all</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -27,6 +29,7 @@
     left: 0;
     width: 100%;
     height: 100%;
+    border-radius: 1%;
     background-image: url('/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp');
     background-size: cover;
     transition: all 0.5s ease-in-out;
@@ -36,4 +39,9 @@
     transform: scale(0.8);
     height:70%;
 }
+
+.backgroundScale:hover .hiddenText {
+    opacity: 1;
+}
+
 </style>
