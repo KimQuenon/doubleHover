4e1d5cce52c165b804ff08dcd43e8801c58e8f71
Kim <kimberley.quenon.@gmail.com>
2024-04-04 10:33:35 +0200

    add img

--- components/DoubleHover.vue
+++ components/DoubleHover.vue
@@ -1,25 +1,22 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 h-full">
-        <div class="bg-white rounded shadow p-4">
-            <!-- <div>
-                <nuxt-img src="/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp"></nuxt-img>
-            </div> -->
+        <div class="w-full h-full rounded-lg overflow-hidden">
+            <img src="/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp" alt="Description de l'image" class="h-full w-full object-cover">
+            <div class="bg-white rounded shadow p-4">
+                <h2 class="text-xl font-semibold mb-2">Card 1</h2>
+                <p class="text-gray-600">Tag 1</p>
+                <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                <button class="bg-red-400 p-2 px-4 rounded-full text-white mt-4" type="button">See all</button>
+            </div>
+        </div>
+        <!-- <div class="bg-white rounded shadow p-4">
+            <div class="h-4/6 object-cover w-full h-full rounded-lg overflow-hidden mb-4">
+                <img src="/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp" alt="Description de l'image" class="h-full w-full object-cover">
+            </div>
             <h2 class="text-xl font-semibold mb-2">Card 1</h2>
             <p class="text-gray-600">Tag 1</p>
             <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
             <button class="bg-red-400 p-2 px-4 rounded-full text-white mt-4" type="button">See all</button>
-        </div>
-        <div class="bg-white rounded shadow p-4">
-            <h2 class="text-xl font-semibold mb-2">Card 1</h2>
-            <p class="text-gray-600">Tag 1</p>
-            <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-            <button class="bg-red-400 p-2 px-4 rounded-full text-white mt-4" type="button">See all</button>
-        </div>
-        <div class="bg-white rounded shadow p-4">
-            <h2 class="text-xl font-semibold mb-2">Card 1</h2>
-            <p class="text-gray-600">Tag 1</p>
-            <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-            <button class="bg-red-400 p-2 px-4 rounded-full text-white mt-4" type="button">See all</button>
-        </div>
+        </div> -->
     </div>
 </template>
