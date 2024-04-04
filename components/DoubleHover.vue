8c0688fffc778186bee32efe378b8fab7362769c
Kim <kimberley.quenon.@gmail.com>
2024-04-04 11:33:21 +0200

    add first hover

--- components/DoubleHover.vue
+++ components/DoubleHover.vue
@@ -1,22 +1,39 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 h-full">
-        <div class="w-full h-full rounded-lg overflow-hidden">
-            <img src="/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp" alt="Description de l'image" class="h-full w-full object-cover">
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Card 1</h2>
-                <p class="text-gray-600">Tag 1</p>
-                <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-                <button class="bg-red-400 p-2 px-4 rounded-full text-white mt-4" type="button">See all</button>
+        <div class="w-full h-full rounded-lg overflow-hidden relative p-2">
+            <div class="bg-white rounded shadow backgroundScale h-full relative">
+                <div class="rounded w-full h-full shadow-lg p-10 z-50 relative">
+                    <div class="w-full h-full">
+                        <h2 class="text-xl font-semibold mb-2">Card 1</h2>
+                        <p class="text-gray-600">Tag 1</p>
+                        <!-- <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p> -->
+                        <button class="bg-red-400 p-2 px-8 rounded-full text-white mt-4" type="button">See all</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <!-- <div class="bg-white rounded shadow p-4">
-            <div class="h-4/6 object-cover w-full h-full rounded-lg overflow-hidden mb-4">
-                <img src="/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp" alt="Description de l'image" class="h-full w-full object-cover">
-            </div>
-            <h2 class="text-xl font-semibold mb-2">Card 1</h2>
-            <p class="text-gray-600">Tag 1</p>
-            <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-            <button class="bg-red-400 p-2 px-4 rounded-full text-white mt-4" type="button">See all</button>
-        </div> -->
     </div>
 </template>
+
+<style>
+.backgroundScale {
+    position: relative;
+}
+
+.backgroundScale::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('/assets/images/qreative-agence-web-nivelles-hiwbi-2.webp');
+    background-size: cover;
+    transition: all 0.5s ease-in-out;
+}
+
+.backgroundScale:hover::before {
+    transform: scale(0.8);
+    height:70%;
+}
+</style>
